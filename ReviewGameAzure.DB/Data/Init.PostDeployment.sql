9e92dbc0e84c4aa89c1648fe0ac2fa472c49f104
Aurélien <aurelien.strimelle@bstorm.be>
2022-10-06 13:31:44 +0200

    Fix data

--- ReviewGameAzure.DB/Data/Init.PostDeployment.sql
+++ ReviewGameAzure.DB/Data/Init.PostDeployment.sql
@@ -3,7 +3,7 @@ SET IDENTITY_INSERT [Category] ON;
 
 INSERT INTO Category ([Id], [Name], [Description])
  VALUES (1, 'Action', NULL),
-		(2, 'Roque Like', NULL),
+		(2, 'Rogue Like', NULL),
 		(3, 'RPG', 'Jeu de role'),
 		(4, 'Simulation', 'Comme dans la réalité, mais en mieux :o');
 
